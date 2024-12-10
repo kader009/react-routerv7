33c79158a5d54d5f43607fcbf6d646b22fb124e0
kader009 <kadermollarasel000@gmail.com>
2024-12-10 20:15:36 +0600

    create about page

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router";
+
 export function Welcome() {
   return (
     <main >
       <h1 className="font-semibold capitalize">hellow v7</h1>
+      <Link to={'/about'}>About</Link>
     </main>
   );
 }
