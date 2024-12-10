8a522e406a41fd94b1eb60c22abdb526035fbfde
kader009 <kadermollarasel000@gmail.com>
2024-12-10 20:42:31 +0600

    fetch data in the new router file

--- app/pages/About.tsx
+++ app/pages/About.tsx
@@ -1,5 +1,20 @@
+import { use } from "react";
+
+async function fetchData() {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const data = await response.json();
+  console.log(data);
+}
+
+const getData = fetchData()
+
 const About = () => {
-  return <div>About</div>;
+  const data = use(getData)
+  return (
+    <div>
+      <h1 className="text-center font-semibold">About</h1>
+    </div>
+  );
 };
 
 export default About;
