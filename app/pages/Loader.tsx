ae9b9453cc4fda96a725a2bf6159e77275a5f4ed
kader009 <kadermollarasel000@gmail.com>
2024-12-11 17:08:45 +0600

    solve the get data issue

--- app/pages/Loader.tsx
+++ app/pages/Loader.tsx
@@ -9,7 +9,14 @@ const Loader = ({ loaderData }: { loaderData: { data: string[] } }) => {
   console.log(loaderData.data);
   return (
     <div>
-      <h1>Loader Data</h1>
+      <h1 className="text-center text-blue-700 font-semibold">Loader Data</h1>
+      <div>
+        {loaderData.data.map((user) => (
+          <div>
+            <h1>{user.name}</h1>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
