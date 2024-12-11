8832de508d00b3f32a597a2f170237227f88f70e
kader009 <kadermollarasel000@gmail.com>
2024-12-11 17:48:35 +0600

    design the data page

--- app/pages/Loader.tsx
+++ app/pages/Loader.tsx
@@ -37,12 +37,13 @@ interface User {
 
 const Loader = ({ loaderData }: { loaderData: { data: User[] } }) => {
   return (
-    <div>
-      <h1 className="text-center text-blue-700 font-semibold">Loader Data</h1>
-      <div>
+    <div className="min-h-screen p-4 bg-gray-50">
+      <h1 className="text-center text-blue-500 font-semibold text-2xl">Loader Data</h1>
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
         {loaderData.data.map((user) => (
-          <div key={user.id}>
+          <div key={user.id} className="bg-white shadow-md rounded-lg p-4 hover:shadow-lg transition-shadow duration-300">
             <h1>{user.name}</h1>
+            <h2>{user.email}</h2>
           </div>
         ))}
       </div>
