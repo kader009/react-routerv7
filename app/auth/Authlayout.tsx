131eff884d61908292644203675ce590796e04f5
kader009 <kadermollarasel000@gmail.com>
2024-12-11 21:02:06 +0600

    create dist file

--- app/auth/Authlayout.tsx
+++ app/auth/Authlayout.tsx
@@ -4,7 +4,7 @@ import Footer from '~/components/Footer';
 const Authlayout = () => {
   return (
     <div className="flex flex-col min-h-screen bg-gray-50">
-      {/* Main Content */}
+      {/* Main Content is here*/}
       <div className="flex-grow flex items-center justify-center">
         <div className="w-full max-w-md bg-white shadow-lg p-6 rounded-lg">
           <Outlet />
