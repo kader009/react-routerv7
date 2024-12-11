3423d795041c56f47bbbde76101c0c3db5294ea5
kader009 <kadermollarasel000@gmail.com>
2024-12-11 19:15:35 +0600

    create laoder file

--- app/pages/ClientLoader.tsx
+++ app/pages/ClientLoader.tsx
@@ -1,8 +1,30 @@
+import type { Route } from '../+types/root';
 
-const ClientLoader = () => {
-  return (
-    <div>ClientLoader</div>
-  )
+export async function clientLoader() {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/todos`);
+  const todo = await res.json();
+  return todo;
 }
 
-export default ClientLoader
+interface Todo {
+  id: number;
+  title: string;
+}
+
+const ClientLoader = ({ loaderData }: { loaderData: Todo[] }) => {
+  return (
+    <div>
+      <h1>ClientData</h1>
+      <div>
+        {loaderData.map((todo) => (
+          <div>
+            <h1>{todo.id}</h1>
+            <h1>{todo.title}</h1>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ClientLoader;
--- app/pages/Loader.tsx
+++ app/pages/Loader.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router';
+
 export async function loader() {
   const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
   if (!response.ok) throw new Error('something went wrong..');
@@ -48,7 +50,10 @@ const Loader = ({ loaderData }: { loaderData: { data: User[] } }) => {
             className="bg-white shadow-md rounded-lg p-4 hover:shadow-lg transition-shadow duration-300"
           >
             <h1>{user.name}</h1>
-            <h2>{user.email}</h2>
+            <h2 className="mb-2">{user.email}</h2>
+            <Link to={''}>
+              <span className="bg-black text-white rounded p-1">see more</span>
+            </Link>
           </div>
         ))}
       </div>
