39843cec4cb32684fd1ea0381c59128a81043bbb
kader009 <kadermollarasel000@gmail.com>
2024-12-11 17:00:30 +0600

    get the loader data

--- app/pages/Loader.tsx
+++ app/pages/Loader.tsx
@@ -1,8 +1,12 @@
 export async function loader() {
-  return { data: 'https://jsonplaceholder.typicode.com/users' };
+  const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+  if (!response.ok) throw new Error('something went wrong..');
+  const data = await response.json();
+  return { data: data };
 }
 
-const Loader = () => {
+const Loader = ({ loaderData }: { loaderData: { data: string[] } }) => {
+  console.log(loaderData.data);
   return (
     <div>
       <h1>Loader Data</h1>
